9e8ea47c6a408569535d3b03246b91b0b54afab8
Anant <anantpathak564@gmail.com>
2025-10-30 19:17:24 +0530

    finally complete  working App after fixing all bugs

--- js/script.js
+++ js/script.js
@@ -16,23 +16,38 @@ function secondsToMinutesSeconds(seconds) {
 
 
 async function getSongs(folder) {
+     folder = folder.replace(/\\/g, "/");
     currFolder = folder;
     let a = await fetch(`http://127.0.0.1:3000/${folder}/`)  // using my port number in online where my  respository are present we are fetching from online
+       console.log(`http://127.0.0.1:3000/${folder}/`) 
     let response = await a.text(); //here my DOM convert in the form of text where my songs are located, we are converting it to take out songs
     let div = document.createElement("div");
     div.innerHTML = response  //store whole dom(html document) where my all songs are presents in table form
     // document.body.append(div) we will not put it into our webpage
     let as = div.getElementsByTagName("a")  // as will now have all a element present in div's innerhtml
+    
     songs = []  // make songs array to store all songs
-    for (let index = 0; index < as.length; index++) {   //we will run for loop to get a element whose href endwith .mp3 it means it is a element who have song link so we store that element's href into songs array
+    for (let index = 0; index < as.length; index++) {
         const element = as[index];
+
         if (element.href.endsWith(".mp3")) {
-            songs.push(element.href.split(`/${folder}/`)[1]);  //this will split my href link into two parts based on given point("folder")left and right
+            console.log(element)
+            
+            const decodedHref = decodeURIComponent(element.href);
+            
+            const normalizedHref = decodedHref.replace(/\\/g, "/");
+            console.log(normalizedHref)
+            console.log(folder)
+            const parts = normalizedHref.split(`/${folder}/`);
+            // const parts = decodedHref.split(`/${folder}/`);
+            console.log(parts)
+            
+
+            songs.push(parts[1]);
         }
     }
-    //show all songs in the playlist
-    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]; //it will return collection of ul even if there is only one ul in songList so to get first index's ul we wrote [0]  or instead of collection one can just return first element using document.querySelector(".songList ul")
-    songUL.innerHTML = ""  // to make sure new song's list will load
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    songUL.innerHTML = ""
     for (const song of songs) {
         songUL.innerHTML = songUL.innerHTML + ` <li>
                             <img class="invert" src="img/music.svg" width="22px" height="22px" alt="">
@@ -59,7 +74,13 @@ async function getSongs(folder) {
 const playMusic = (track, pause = false) => {
     // let audio = new Audio ("/songs/" + track)      // they will make new object again and again that causes multiple songs play at one time
     // audio.play()
-    currentSong.src = `/${currFolder}/` + track  // it will send request to songs folder to get track(means it will become something like this https://127.0.0.1/3000/songs/track) then it will give me that track's link from songs folder and put into currentSong.src means in Audio object's src to play
+    console.log(`/${currFolder}/` + track )
+    
+    currentSong.src = `/${currFolder}/` + track  // it will send request to songs folder to get track(means it will become something like 
+    
+
+    console.log(currentSong.src)
+    // this https://127.0.0.1/3000/songs/track) then it will give me that track's link from songs folder and put into currentSong.src means in Audio object's src to play
     if (!pause) {
         currentSong.play();
         play.src = "img/pause.svg"
@@ -70,55 +91,79 @@ const playMusic = (track, pause = false) => {
 
 
 }
+
 async function displayAlbums() {
-    let a = await fetch(`http://127.0.0.1:3000/songs/`)  //this will give me my songs whole folder into stream of byts
-    let response = await a.text();  //this will convert my stream of byts into text which seems like html document
+    console.log("displaying albums");
+
+    // Fetch the main songs directory
+    let a = await fetch(`http://127.0.0.1:3000/songs/`);
+    let response = await a.text();
+
     let div = document.createElement("div");
-    div.innerHTML = response
-    let anchors = div.getElementsByTagName("a")
-    let cardContainer = document.querySelector(".cardContainer")
+    div.innerHTML = response;
 
+    let anchors = div.getElementsByTagName("a");
+    let cardContainer = document.querySelector(".cardContainer");
+    cardContainer.innerHTML = ""; // clear old content
+
+    // Convert to array for easier iteration
     let array = Array.from(anchors);
-    for (let index = 0; index < array.length; index++) {
-        const e = array[index];
 
-        if (e.href.includes("/songs")) {
-            let folder = e.href.split("/").slice(-2)[0]
+    for (let e of array) {
+        const decoded = decodeURIComponent(e.href).replace(/\\/g, "/");
 
-            //get metadata of folder
-            let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)  //all folders have info.json which holds meta data of folder
-            let response2 = await a.json();
-            cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="${folder}" class="card">
+        // Include only folders under /songs/
+        if (decoded.includes("/songs/") && !decoded.endsWith(".htaccess") && !decoded.endsWith(".mp3")) {
+            // Extract the folder name correctly
+            let parts = decoded.split("/songs/");
+            let folder = parts[1].replace(/\/$/, ""); // remove trailing slash if any
+
+            console.log("Detected folder:", folder);
+
+            // Fetch info.json of each folder
+            try {
+                let infoResponse = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`);
+                if (!infoResponse.ok) continue; // skip if info.json not found
+
+                let info = await infoResponse.json();
+
+                // Add card HTML
+                cardContainer.innerHTML += `
+                    <div data-folder="${folder}" class="card">
                         <div class="play">
                             <img src="img/spoti.svg" alt="Play Icon" width="20px">
                         </div>
-                        <img src="/songs/${folder}/cover.jpg" alt="">
-                        <h2>${response2.title}</h2>
-                        <p>${response2.description}</p>
-                    </div>`
-
-
+                        <img src="songs/${folder}/cover.jpg" alt="not found">
+                        <h2>${info.title}</h2>
+                        <p>${info.description}</p>
+                    </div>`;
+            } catch (err) {
+                console.warn("Skipping folder (no info.json):", folder);
+            }
         }
     }
-    //load playlist whenever card is clicked
-    Array.from(document.getElementsByClassName("card")).forEach(e => {
-        e.addEventListener("click", async item => {
-            await getSongs(`songs/${item.currentTarget.dataset.folder}`)
-            playMusic(songs[0])
-        })
-    })
+
+    // Add click event to load songs on card click
+    Array.from(document.getElementsByClassName("card")).forEach(card => {
+        card.addEventListener("click", async (e) => {
+            let folder = e.currentTarget.dataset.folder;
+            await getSongs(`songs/${folder}`);
+            console.log(songs[0])
+            playMusic(songs[0]);
+        });
+    });
 }
 
 
-async function main() {     //we create async function main because we don't want promise we want direct array for that we have to await for getsongs and for await i have to create async function
-    //get the list of all songs
-    await getSongs("songs/ncs")    //it will now not return promise pending or some thing it will wait for songs array and then store in variable songs then print
+
+
+async function main() {    
+    await getSongs("songs/ncs")    
     playMusic(songs[0], true)
 
-    //display all songs on the page
+    
     displayAlbums()
-    //Attach an event listener to play, next, and previous
-    // let play = document.querySelector(".songbuttons>.play");
+    
     play.addEventListener("click", () => {
         if (currentSong.paused) {
             currentSong.play();
@@ -137,8 +182,7 @@ async function main() {     //we create async function main because we don't wan
     })
     //Add an event listener to seekbar
     document.querySelector(".seekbar").addEventListener("click", e => {
-        // console.log(e.offsetX) //offsetX means distance in x direction according to viewport, every click event in different position has different properties
-        // The getBoundingClientRect() method returns the size of an element and its position relative to the viewport
+        
         let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
         document.querySelector(".circle").style.left = percent + "%"; // it will move my circle in that much percent in x direction by increasing left
         currentSong.currentTime = (currentSong.duration * percent) / 100; // it will forward my song that much percent from total duration
@@ -155,7 +199,9 @@ async function main() {     //we create async function main because we don't wan
     })
     //Add event listener to previous
     previous.addEventListener("click", () => {
-        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
+        console.log(songs.indexOf(currentSong.src.split("/").slice(-1)[0]))
+        let previousSong =  currentSong.src.split("/").slice(-1)[0].replaceAll("%20", " ")
+        let index = songs.indexOf(previousSong);
         if (index - 1 >= 0) {   //should execute till 0th index
             playMusic(songs[index - 1]);
         }
@@ -163,7 +209,10 @@ async function main() {     //we create async function main because we don't wan
     })
     //Add event listener to next
     next.addEventListener("click", () => {
-        let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
+        // let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
+        let nextSong = currentSong.src.split("/").slice(-1)[0].replaceAll("%20", " ")
+        let index = songs.indexOf(nextSong);
+        
         if (index + 1 < songs.length) {  //should execut till last index of songs array
             playMusic(songs[index + 1]);
         }
@@ -172,32 +221,32 @@ async function main() {     //we create async function main because we don't wan
     document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e) => {
         console.log("Your Music's volume is", e.target.value, "/100")
         currentSong.volume = parseInt(e.target.value) / 100;   //volume attribute take only value from 0 to 1 that's why /100
-        if(currentSong.volume >0){
+        if (currentSong.volume > 0) {
             document.querySelector(".volume>img").src = document.querySelector(".volume>img").src.replace("mute.svg", "volume.svg")
         }
     })
 
     //add event listner to mute the track
-    document.querySelector(".volume>img").addEventListener("click", e=>{
+    document.querySelector(".volume>img").addEventListener("click", e => {
         console.log(e.target)
-        if(e.target.src.includes("volume.svg")){
+        if (e.target.src.includes("volume.svg")) {
             console.log("im clicking the volume")
             e.target.src = e.target.src.replace("volume.svg", "mute.svg")
-            currentSong.volume =0;
+            currentSong.volume = 0;
             document.querySelector(".range").getElementsByTagName("input")[0].value = 0;
         }
-        else{
+        else {
             e.target.src = e.target.src.replace("mute.svg", "volume.svg")
             currentSong.volume = .10
             document.querySelector(".range").getElementsByTagName("input")[0].value = 50;
-            
+
         }
     })
 
 
 
 
- 
+
 
 
 
